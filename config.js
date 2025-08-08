b8f66b2d519d7a860b36c8588d7dd796384f435b
Sadpub100 <sad.pub0582@gmail.com>
2025-08-08 09:39:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~Iy0FzRxI#okaRmhUvOVh2005ff7cymqeaFqs-fBm6zOM8j6-zpBc ",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  PREFIX: "•" // prefix (e.g., ., /, !, *)
 };
